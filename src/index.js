d033beaf2d3a51094d29810f60f4e5918242d266
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2019-07-30 16:58:20 +0200

    feat Routing Dev du routing ajout de firebase

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 
-function Navbar(){
+function AppRouter(){
     return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="#">Accueil <span class="sr-only"></span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Films</a></li>
+        <Router>
+        <nav className="navbar navbar-expand-sm bg-dark">
+                <ul className="navbar-nav">
+                    <li className="nav-item"><Link to="/" className="nav-link">Accueil</Link></li>
+                    <li className="nav-item"><Link to="/films" className="nav-link">Films</Link></li>
                 </ul>
-            </div>
         </nav>
-    )
+
+            <Route path="/"  exact component={Accueil} />
+            <Route path="/films" component={Films} />
+        </Router>
+    );
+}
+
+function Accueil(){
+    return (
+        <div>
+        <h1>Accueil</h1>
+        </div>
+    );
+}
+
+function Films(){
+    return(
+        <div>
+        <h1>Liste des films : </h1>
+        </div>
+    );
 }
 
 function App(){
     return(
-        Navbar()
+        AppRouter()
     );
 }
+
+export default AppRouter;
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
